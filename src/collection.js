68ad0e5cd998d82e2311252cc0bde0a008cf20b7
Ben Drucker <bvdrucker@gmail.com>
2014-11-05 11:39:17 -0500

    Generate collection refs

--- src/collection.js
+++ src/collection.js
@@ -2,6 +2,12 @@
 
 module.exports = function (ConvexCollection, Firebase) {
 
+  ConvexCollection.prototype.$ref = function () {
+    return this.$$parent.$ref(false);
+  };
+
+  return ConvexCollection;
+
 };
 
 module.exports.$inject = ['$delegate', 'Firebase'];
--- test/collection.js
+++ test/collection.js
@@ -2,4 +2,31 @@
 
 module.exports = function () {
 
+  beforeEach(angular.mock.module(function ($provide) {
+    $provide.value('Firebase', require('mockfirebase').MockFirebase);
+  }));
+
+  var Firebase, $timeout, Model, model, collection;
+  beforeEach(angular.mock.inject(function ($injector) {
+    Firebase = $injector.get('Firebase');
+    $timeout = $injector.get('$timeout');
+
+    var ConvexModel = $injector.get('ConvexModel');
+    var ConvexCollection = $injector.get('ConvexCollection');
+    Model = ConvexModel.extend({
+      $name: 'user'
+    });
+    model = new Model();
+    collection = new ConvexCollection(Model);
+  }));
+
+  describe('#$ref', function () {
+
+    it('can get a reference from its parent model', function () {
+      collection.$relate('user', model);
+      expect(collection.$ref().currentPath).to.equal('Mock://users');
+    }); 
+
+  });
+
 };
